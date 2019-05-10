68539b61db7398bafd5251c4781e9105e80dffad
Vorodin <volodymyr.rodin@gmail.com>
2019-05-10 06:24:38 -0700

    Codes removed

--- Codes/machine-learning-octave-master/machine-learning-octave-master/anomaly-detection/estimate_gaussian.m
+++
@@ -1,7 +0,0 @@
-% This function estimates the parameters of a Gaussian distribution using the data in X.
-function [mu sigma2] = estimate_gaussian(X)
-    % Useful variables
-    [m, n] = size(X);
-    mu = mean(X);
-    sigma2 = (1 / m) * sum((X - mu) .^ 2);
-end
